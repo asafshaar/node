29aa37e8fb68bb5ee2a73cb67f3f2aba00a69f7e
Asaf Shaar <asafs@empow.co>
2020-05-14 17:36:52 +0300

    main 2 for new branch only

---
+++ main2.js
@@ -0,0 +1,13 @@
+const {add, sub, fix} = require('./math');
+console.log(name);
+var name = "meni";
+console.error(fix);
+
+prompt("www");
+var res1 = add(7,9);
+console.log(res1);
+
+var sub1 = sub(246, 46);
+console.log(sub1);
+
+
